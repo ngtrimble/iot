df5b66107251fcb31b4fb217ae538107613d29e2
Nate Trimble <904927+ngtrimble@users.noreply.github.com>
2025-11-18 16:35:47 -0800

    updates

--- self-managed/deploy.sh
+++ self-managed/deploy.sh
@@ -9,10 +9,13 @@ mosquitto_passwd -c -b passwd iotdev abcd1234
 kubectl delete secret -n mosquitto mosquitto-passwd
 kubectl create secret generic -n mosquitto mosquitto-passwd --from-file=passwd 
 
-# Deploy MQTT Mosquitto
+# Deploy Mosquitto
 kubectl apply -f mosquitto.yaml
 
-# Deploy InfluxDB
-helm repo add influxdata https://helm.influxdata.com/
-help repo update
-helm upgrade influxdb influxdata/influxdb --install -f influxdb-values.yaml --namespace influxdb --create-namespace
+# Deploy InfluxDB 3, caveat, this is Enteprise and not what is wanted. Core is more complex to install
+# helm repo add influxdata https://helm.influxdata.com/
+# help repo update
+# helm upgrade influxdb influxdata/influxdb3-clustered --install -f  --namespace influxdb --create-namespace
+
+# Deploy InfluxDB 3 Core
+kubectl apply -f influxdb3-core.yaml
